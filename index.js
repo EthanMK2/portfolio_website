38fbec715b506b5e121f4d79207e23a0d4f5af60
Ethan Klukkert <ethan7klukkert@gmail.com>
2023-03-25 19:10:44 -0400

    front page animations

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const logSomething = () => {
-    console.log("LOG, hello.")
+const letters = document.getElementsByClassName("jump");
+
+// recurring animation
+setInterval(() => {
+  for (let i = 0; i < letters.length; i++) {
+    setTimeout(() => {
+      letters[i].classList.add("jump-animation");
+      setTimeout(() => {
+        letters[i].classList.remove("jump-animation");
+      }, 1000);
+    }, i * 100);
+  }
+}, 6000);
+
+// "hover" animation
+for (let i = 0; i < letters.length; i++) {
+  letters[i].addEventListener("mouseenter", () => {
+    letters[i].classList.add("jump-animation");
+    setTimeout(() => {
+      letters[i].classList.remove("jump-animation");
+    }, 400);
+  });
 }
