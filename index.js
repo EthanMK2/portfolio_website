35ab9cba669853c93a5841a10e703f3b94c3c26a
Ethan Klukkert <ethan7klukkert@gmail.com>
2023-03-28 19:38:14 -0400

    Add intro animations and header styles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const letters = document.getElementsByClassName("jump");
+const subtitle = document.getElementById("subtitle");
 
-// recurring animation
+// ||||||||||||||||| recurring animation on title |||||||||||||||||
 setInterval(() => {
   for (let i = 0; i < letters.length; i++) {
     setTimeout(() => {
@@ -10,9 +11,9 @@ setInterval(() => {
       }, 1000);
     }, i * 100);
   }
-}, 6000);
+}, 5000);
 
-// "hover" animation
+// ||||||||||||||| "hover" animation on title |||||||||||||||
 for (let i = 0; i < letters.length; i++) {
   letters[i].addEventListener("mouseenter", () => {
     letters[i].classList.add("jump-animation");
@@ -21,3 +22,40 @@ for (let i = 0; i < letters.length; i++) {
     }, 400);
   });
 }
+
+// ||||||||||||||| intro animation on subtitle ||||||||||||||||
+subtitle.classList.add("title__subtitle-intro-animation");
+setTimeout(() => {
+  subtitle.classList.remove("title__subtitle-intro-animation")
+  subtitle.classList.add("title__subtitle-after-intro")
+}, 1500)
+
+// |||||||||||||||| scroll animations |||||||||||||||||||
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("section_title-animation");
+      return
+    }
+
+    //entry.target.classList.remove("section_title-animation")
+  })
+})
+
+document.querySelectorAll(".section_title").forEach((element) => {
+  if (element) {
+    observer.observe(element)
+  }
+})
+
+document.querySelectorAll(".description").forEach((element) => {
+  if (element) {
+    observer.observe(element)
+  }
+})
+
+document.querySelectorAll(".project").forEach((element) => {
+  if (element) {
+    observer.observe(element)
+  }
+})
